f62e5b43cb0b81da1379ca3b8a8a607d9f618837
Mitch Ferrer <mferrer@appriver.com>
2015-04-24 15:20:58 -0600

    cleanup

--- TicTacToeKata.TypeScript/SquareState.js
+++
@@ -1,7 +0,0 @@
-var Marker;
-(function (Marker) {
-    Marker[Marker["Empty"] = 0] = "Empty";
-    Marker[Marker["X"] = 1] = "X";
-    Marker[Marker["O"] = 2] = "O";
-})(Marker || (Marker = {}));
-//# sourceMappingURL=SquareState.js.map
